e6d537d2d56520d571c23bf803cf699f33743e9d
chumbam <chumbam@mail.ru>
2022-04-04 16:00:37 +0300

    create navigation

--- app/src/main/java/com/example/newsapptest/ui/MainNewsActivity.kt
+++ app/src/main/java/com/example/newsapptest/ui/MainNewsActivity.kt
@@ -2,7 +2,11 @@ package com.example.newsapptest.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentContainerView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.newsapptest.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainNewsActivity : AppCompatActivity() {
@@ -10,8 +14,10 @@ class MainNewsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_news)
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        val newsNavHostContainer: FragmentContainerView = findViewById(R.id.newsNavHostFragment)
 
-
+        bottomNavigationView.setupWithNavController(newsNavHostContainer.findNavController())
     }
 
 
